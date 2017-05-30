98656ee8d81c168c2cd39bdc8872110721e3ef20
Roman Schilter <roman.schilter@internezzo.ch>
2017-05-30 09:02:25 +0200

    Added commit to wirte this here

--- Classes/Controller/MiniController.php
+++ Classes/Controller/MiniController.php
@@ -10,8 +10,32 @@ use Neos\Flow\Mvc\Controller\ActionController;
 
 class MiniController extends ActionController
 {
+	/**
+	 * @Flow\Inject
+	 * @var \Neos\Flow\Security\Context
+	 */
+	protected $securityContext;
+
+	/**
+	 * @Flow\Inject
+	 * @var \schilter\gw2challenges\Domain\Repository\MiniRepository
+	 */
+	protected $miniRepository;
+
+	/**
+	 * @Flow\Inject
+	 * @var \schilter\gw2challenges\Domain\Repository\AccountRepository
+	 */
+	protected $accountRepository;
 	
 	public function indexAction(){
-		
+		if($this->securityContext->getAccount()){
+			$minis = $this->securityContext->getAccount()->getMinis();
+		}
+		else{
+			$minis = $this->miniRepository->findAll()->toArray();
+		}
+
+		$this->view->assign('minis', json_encode($minis));
 	}
 }
