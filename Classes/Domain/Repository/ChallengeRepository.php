1d5267a2998442c92081d502b64be1aa9a9bf729
Erdgigant <roman.schilter10@gmail.com>
2017-06-11 15:20:20 +0200

    sadsad

--- Classes/Domain/Repository/ChallengeRepository.php
+++ Classes/Domain/Repository/ChallengeRepository.php
@@ -17,6 +17,12 @@ class ChallengeRepository {
 	 */
 	protected $pdoService;
 	
+	/**
+	 * @Flow\Inject
+	 * @var \Neos\Flow\Property\PropertyMapper
+	 */
+	protected $propertyMapper;
+	
 	public function getById($id){
 		$stmt = $this->pdoService->getPdo()->prepare("SELECT * FROM schilter_gw2challenges_domain_model_challenge WHERE id =".$id);
 		$stmt->execute();
@@ -24,6 +30,30 @@ class ChallengeRepository {
 				$stmt->fetch(),
 				\schilter\gw2challenges\Domain\Model\Challenge::class,
 				$this->getConfiguration());
+	}
+	
+	public function newChallenge($name, $ids){
+		$this->pdoService->getPdo()->beginTransaction();
+		$challenge = $this->propertyMapper->convert(
+				array(
+						'name' => $name,
+						'minis' => $ids
+				),\schilter\gw2challenges\Domain\Model\Challenge::class, $this->getConfiguration());
+		$identifier =  \Neos\Utility\ObjectAccess::getProperty($challenge, 'Persistence_Object_Identifier', true);
+		$sql = 'INSERT INTO schilter_gw2challenges_domain_model_challenge (persistence_object_identifier, name, minis) 
+				VALUES (\''.$identifier.'\', \''.$challenge->getName().'\', \''.$challenge->getMinis().'\')';
+		$stmt = $this->pdoService->getPdo()->prepare($sql);
+		$stmt->execute();
+		
+		$this->pdoService->getPdo()->commit();
+		return $identifier;
+	}
+	
+	public function getIdByIdentifier($identifier){
+		$sql = 'SELECT id FROM schilter_gw2challenges_domain_model_challenge WHERE persistence_object_identifier =\''.$identifier.'\'';
+		$stmt = $this->pdoService->getPdo()->prepare($sql);
+		$stmt->execute();
+		return $stmt->fetch()['id'];
 	}
 	
 	public function getConfiguration()
